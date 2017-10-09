b4098491a9476fc7cf02cee90ebd75fe1054f11e
lucas steuperaert <lucas.steuperaert@gmail.com>
2017-10-09 11:55:18 +0200

    fix javascript

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,14 +1,12 @@
-$(document).ready(function() {
-  $('.menu').hide();
-   $('#work-phone').hide().show( "drop", { direction: "down"}, 400 );
-  $('.mockup').hide().show( "drop", { direction: "down"}, 500 );
-});
-
 function openNav() {
   $('.menu').toggle( "drop", { direction: "right"}, 250 );
+  $('#about').toggleClass('opacity-low');
+  $('#contact').toggleClass('opacity-low');
+  $('#home').toggleClass('opacity-low');
+  $('#work').toggleClass('opacity-low');
   $('#calypso').toggleClass('opacity-low');
   $('.ui-menu__line_1').toggleClass('ui-menu__line_1_opened');
   $('.ui-menu__line_3').toggleClass('ui-menu__line_3_opened');
   $('.ui-menu__line_2').toggle();
   $("body").toggleClass('no-scroll');
-  };
+};
