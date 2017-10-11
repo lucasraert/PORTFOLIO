819d4d3a8c1ee1f08dd99511a3c29e3f5f8f3297
lucas steuperaert <lucas.steuperaert@gmail.com>
2017-10-11 15:30:17 +0200

    remove useless html css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
   get 'hire', to: 'pages#hire'
-  get 'work', to: 'pages#work'
   get 'calypso', to: 'pages#calypso'
-  get 'contact', to: 'pages#contact'
-  get 'home2', to: 'pages#home2'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
